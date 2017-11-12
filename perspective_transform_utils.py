f908c88234525669652ba5921cbbcd9f8fc76dbc
Gary Wu <gary.wu@airbnb.com>
2017-11-11 22:00:19 -0800

    clean up

--- perspective_transform_utils.py
+++ perspective_transform_utils.py
@@ -3,10 +3,7 @@ import numpy as np
 
 src = np.float32(np.array([[550, 477], [160, 720], [1120, 720], [730, 477]]))
 dst = np.float32(np.array([[320, 0], [320, 720], [960, 720], [960, 0]]))
-#dst = np.float32([[520, 460], [520, 720], [760, 720], [760, 460]])
 
-#src = np.float32(np.array([[581, 477], [699, 477], [896, 675], [384, 675]]))
-#dst = np.float32(np.array([[384, 0], [896, 0], [896, 720], [384, 720]]))
 M = cv2.getPerspectiveTransform(src, dst)
 M_inverse = cv2.getPerspectiveTransform(dst, src)
 
